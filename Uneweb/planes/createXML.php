151d8d4d41d13a20ce8148ebc010bc5bfe7eafdb
felipe.rojasg <felipe.rojasg@gmail.com>
2012-08-16 22:04:41 +0000

    nuevos cambios

--- Uneweb/planes/createXML.php
+++ Uneweb/planes/createXML.php
@@ -12,11 +12,168 @@
 		$seccion = $doc->createElement("seccion");
 		
 		if($i == 0){
+			//es la seccion del itinerario
+			//obtenemos el titulo de cabecera
+			$tmpNode = $doc->createElement("titulo", $_POST["tituloSeccion1"]);
+			$seccion->appendChild($tmpNode);
 			
+			//verificamos si existe imagen para la cabecera
+			if($_FILES["imageSeccion1"]["error"] == 0){
+				//tenemos imagen, la copiamos al directorio respectivo y almacenamos la referencia
+				$tmpNode = $doc->createElement("img", "img/".$_FILES["imageSeccion1"]["name"]);
+			}else {
+				$tmpNode = $doc->createElement("img");
+			}
+			$seccion->appendChild($tmpNode);
+			
+			//recorremos la informacion de los días
+			if(isset($_POST['dayTitle'])){
+				$dayTitles = $_POST['dayTitle'];
+				$dayActivities = $_POST['dayDesc'];
+				$dayImgs = $_FILES["dayImage"]["name"];
+					
+				reset($dayTitles);
+					
+				$subSeccionDias = $doc->createElement("dias");
+				while($dayTitle = each($dayTitles)){
+					//recorremos los dias para crear su info
+					$dia = $doc->createElement("dia");
+					$dia->appendChild($doc->createElement("title", $dayTitle['value']));
+					$dia->appendChild($doc->createElement("actividades", $dayActivities[$dayTitle['key']]));
+					$dia->appendChild($doc->createElement("img", $dayImgs[$dayTitle['key']]));
+				
+					$subSeccionDias->appendChild($dia);
+				}
+				$seccion->appendChild($subSeccionDias);
+			}
+		} else if($i == 1){
+			//es la seccion de que incluye
+			//obtenemos el titulo de cabecera
+			$tmpNode = $doc->createElement("titulo", $_POST["tituloSeccion2"]);
+			$seccion->appendChild($tmpNode);
+			
+			//verificamos si existe imagen para la cabecera
+			if($_FILES["imageSeccion2"]["error"] == 0){
+				//tenemos imagen, la copiamos al directorio respectivo y almacenamos la referencia
+				$tmpNode = $doc->createElement("img", "img/".$_FILES["imageSeccion2"]["name"]);
+			}else {
+				$tmpNode = $doc->createElement("img");
+			}
+			$seccion->appendChild($tmpNode);
+			
+			//recorremos la informacion de lo que se incluye
+			if(isset($_POST['opcionSeccion2'])){
+				$items = $_POST['opcionSeccion2'];
+				reset($items);
+					
+				$subSeccionQueIncluye = $doc->createElement("queincluye");
+				while($item = each($items)){
+					//recorremos los elementos a incluir para crear su info
+					$tmpNode = $doc->createElement("item", $item['value']);
+				
+					$subSeccionQueIncluye->appendChild($tmpNode);
+				}
+				$seccion->appendChild($subSeccionQueIncluye);
+			}
+		} else if($i == 2){
+			//es la seccion de condiciones
+			//obtenemos el titulo de cabecera
+			$tmpNode = $doc->createElement("titulo", $_POST["tituloSeccion3"]);
+			$seccion->appendChild($tmpNode);
+			
+			//verificamos si existe imagen para la cabecera
+			if($_FILES["imageSeccion3"]["error"] == 0){
+				//tenemos imagen, la copiamos al directorio respectivo y almacenamos la referencia
+				$tmpNode = $doc->createElement("img", "img/".$_FILES["imageSeccion3"]["name"]);
+			}else {
+				$tmpNode = $doc->createElement("img");
+			}
+			$seccion->appendChild($tmpNode);
+			
+			//recorremos la informacion de las condiciones
+			if(isset($_POST['opcionSeccion3'])){
+				$items = $_POST['opcionSeccion3'];
+				reset($items);
+					
+				$subSeccionCondiciones = $doc->createElement("condiciones");
+				while($item = each($items)){
+					//recorremos las condiciones para crear su info
+					$tmpNode = $doc->createElement("item", $item['value']);
+				
+					$subSeccionCondiciones->appendChild($tmpNode);
+				}
+				$seccion->appendChild($subSeccionCondiciones);
+			}
+			
+			//recorremos la informacion de los gastos de anulacion
+			if(isset($_POST['gastoSeccion3'])){
+				$items = $_POST['gastoSeccion3'];
+				reset($items);
+					
+				$subSeccionGastos = $doc->createElement("gastos");
+				while($item = each($items)){
+					//recorremos los gastos para crear su info
+					$tmpNode = $doc->createElement("item", $item['value']);
+			
+					$subSeccionGastos->appendChild($tmpNode);
+				}
+				$seccion->appendChild($subSeccionGastos);
+			}
+		} else if($i == 3){
+			//es la seccion de fotos
+			//obtenemos el titulo de cabecera
+			$tmpNode = $doc->createElement("titulo", $_POST["tituloSeccion4"]);
+			$seccion->appendChild($tmpNode);
+			
+			//recorremos la informacion de las fotos
+			if(isset($_FILES['fileSeccion4'])){
+				$items = $_FILES['fileSeccion4']['name'];
+				reset($items);
+					
+				$subSeccionFotos = $doc->createElement("fotos");
+				while($item = each($items)){
+					//recorremos las fotos para crear su info
+					$tmpNode = $doc->createElement("item", $item['value']);
+				
+					$subSeccionFotos->appendChild($tmpNode);
+				}
+				$seccion->appendChild($subSeccionFotos);
+			}
+		} else if($i == 4){
+			//es la seccion de hoteles
+			//obtenemos el titulo de cabecera
+			$tmpNode = $doc->createElement("titulo", $_POST["tituloSeccion5"]);
+			$seccion->appendChild($tmpNode);
+			
+			//verificamos si existe imagen para la cabecera
+			if($_FILES["imageSeccion5"]["error"] == 0){
+				//tenemos imagen, la copiamos al directorio respectivo y almacenamos la referencia
+				$tmpNode = $doc->createElement("img", "img/".$_FILES["imageSeccion5"]["name"]);
+			}else {
+				$tmpNode = $doc->createElement("img");
+			}
+			$seccion->appendChild($tmpNode);
+			
+			//recorremos la informacion de las fotos
+			if(isset($_POST['text1'])){
+				$texto1 = $_POST['text1'];
+				reset($texto1);
+					
+				$subSeccionFilas = $doc->createElement("filas");
+				while($item = each($texto1)){
+					//recorremos las filas para crear su info
+					$fila = $doc->createElement("fila");
+					$fila->appendChild($doc->createElement("ciudad", $item['value']));
+					$fila->appendChild($doc->createElement("hotel", $_POST['text2'][$item['key']]));
+					$fila->appendChild($doc->createElement("categoria", $_POST['text3'][$item['key']]));
+					$subSeccionFilas->appendChild($fila);
+				}
+				$seccion->appendChild($subSeccionFilas);
+			}
 		}
 		
 		$rootNode->appendChild($seccion);
 	}
 	
-	echo $doc->saveXML();
+	echo $doc->save("prueba.xml");
 ?>
---
+++ Uneweb/planes/loadXML.php
@@ -0,0 +1,144 @@
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Editor</title>
+    <link href="css/bootstrap.min.css" type="text/css" rel="stylesheet" />
+    <link href="css/custom-theme/jquery-ui-1.8.18.custom.css" type="text/css" rel="stylesheet" />
+	<link href="css/dia1.css" type="text/css" rel="stylesheet" />
+	<script type="text/javascript" src="js/editor.js"></script>
+	<script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
+    <script type="text/javascript" src="js/js/jquery-ui-1.8.18.custom.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/StickyScroller.js"></script>
+    <script type="text/javascript" src="js/GetSet.js"></script>
+	<script type="text/javascript" src="js/home.js"></script>
+	
+	<style>
+		input {
+			font-size-adjust: none;
+		}
+	</style>
+</head>
+<body>
+	<div id="tabs">
+		<ul>
+			<li><a href="#tabs-1">Itinerario</a></li>
+			<li><a href="#tabs-2">Que incluye</a></li>
+			<li><a href="#tabs-3">Condiciones</a></li>
+			<li><a href="#tabs-4">Fotos</a></li>
+			<li><a href="#tabs-5">Destinos</a></li>
+        </ul>
+        
+        <br />
+        
+        <?php 
+        	$doc = new DOMDocument();
+  			$doc->load('prueba.xml');
+        	
+  			$secciones = $doc->getElementsByTagName("seccion");
+  			$i = 1;
+        	//iteramos sobre las secciones
+			foreach($secciones as $seccion ){
+				if($i == 1){
+		?>
+					<div id="tabs-1">
+						<h3><?php echo $seccion->getElementsByTagName("titulo")->item(0)->nodeValue; ?></h3>
+						<?php 
+						    //vemos si hay imagen que agregar
+						    if($seccion->getElementsByTagName("img")->item(0)->nodeValue){
+						?>
+								<hr width="100%" size="2" /> 
+								<img src="<?php echo $seccion->getElementsByTagName("img")->item(0)->nodeValue?>"/> 
+						<?php
+						    }
+						    
+						    //iteramos sobre la informacion de los dias
+						    $dias = $seccion->getElementsByTagName("dia");
+						    
+						    foreach($dias as $dia ){
+						 ?>
+						 		<hr width="100%" size="2" />
+						    	<br />
+						    	<div class="span4">
+						    		<h4><?php echo $dia->getElementsByTagName("title")->item(0)->nodeValue;?></h4>
+						    		<p><?php echo $dia->getElementsByTagName("actividades")->item(0)->nodeValue;?></p>
+						    	</div>
+						    	<?php 
+						    		if($dia->getElementsByTagName("img")->item(0)->nodeValue){
+						    	?>
+						    			<div class="span3">
+						    				<img src="<?php echo $dia->getElementsByTagName("img")->item(0)->nodeValue;?>"/>
+						    				<br />
+						    				<br />
+						    			</div>
+						    	<?php
+						    		}
+						    	?>
+						 <?php
+						    }
+						?>
+					</div>
+		<?php 
+				} else if($i == 2){
+		?>
+					<div id="tabs-2">
+						<h3><?php echo $seccion->getElementsByTagName("titulo")->item(0)->nodeValue; ?></h3>
+						<?php 
+						    //vemos si hay imagen que agregar
+						    if($seccion->getElementsByTagName("img")->item(0)->nodeValue){
+						?>
+								<hr width="100%" size="2" /> 
+								<img src="<?php echo $seccion->getElementsByTagName("img")->item(0)->nodeValue?>"/> 
+						<?php
+						    }
+						?>
+						<br /> 
+                    	<br />
+                    	<ul>
+                    		<?php 
+                    			//iteramos sobre la informacion de lo que se incluye
+                    			$items = $seccion->getElementsByTagName("item");
+                    			foreach ($items as $item){
+                    				echo "<li>".$item->nodeValue."</li>";
+                    			}
+                    		?>
+                    	</ul> 
+                        <br />
+                        <br />
+                    </div> 
+		<?php
+				} else if($i == 3){
+		?>
+					<div id="tabs-3">
+						<h3><?php echo $seccion->getElementsByTagName("titulo")->item(0)->nodeValue; ?></h3>
+						<?php 
+						    //vemos si hay imagen que agregar
+						    if($seccion->getElementsByTagName("img")->item(0)->nodeValue){
+						?>
+								<hr width="100%" size="2" /> 
+								<img src="<?php echo $seccion->getElementsByTagName("img")->item(0)->nodeValue?>"/> 
+						<?php
+						    }
+						?>
+						<br /> 
+                    	<br />
+                    	<ul>
+                    		<?php 
+                    			//iteramos sobre la informacion de las condiciones
+                    			$items = $doc->xpath('//condiciones//item');
+                    			$items = $seccion->getElementsByTagName("item");
+                    			foreach ($items as $item){
+                    				echo "<li>".$item->nodeValue."</li>";
+                    			}
+                    		?>
+                    	</ul>
+					</div>
+		<?php
+				}
+        		
+				$i++;
+       		}
+        ?>
+	</div>
+</body>
+</html>
