5155a05e5c89d06ce0b36777128ee5902bdf16dc
felipe.rojasg@gmail.com <felipe.rojasg@gmail.com@deb3a35b-e1a3-a7ec-7b53-b241804db672>
2012-08-28 21:09:39 +0000

    colocacion del archivo servicios2.php para emular la carga en el propio servidor

---
+++ trunk/Uneweb/servicios2.php
@@ -0,0 +1,7 @@
+<div style="height:5px;"></div>
+	   		     <hr width="100%" size="2" />
+				<br />
+				<iframe width="720" scrolling="auto" height="3850" frameborder="0" marginheight="0" marginwidth="0" src="planes/loadXML.php?id=<?php echo $_GET[id];?>"></iframe>
+     
+<div style="height:100px; clear: both;"></div>
+		
