859acd56a31a8ecf5cd5a6627a264a980c9f0895
felipe.rojasg@gmail.com <felipe.rojasg@gmail.com@deb3a35b-e1a3-a7ec-7b53-b241804db672>
2012-09-17 19:54:37 +0000

    Galeria dejada con el nivoSlider, solo se quito el atributo data-transition para que las imagenes usen uno aleatorio

--- trunk/Uneweb/planes/nivo-slider/demo/showGallery.php
+++ trunk/Uneweb/planes/nivo-slider/demo/showGallery.php
@@ -1,47 +1,49 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <html lang="en">
 <head>
     <title>Nivo Slider Demo</title>
-    <script type="text/javascript" src="scripts/jquery-1.7.1.min.js"></script>
-    <script type="text/javascript" src="scripts/jquery.cycle.all.js"></script>
-    <script type="text/javascript" src="scripts/jquery.easing.1.3.js"></script>
-    <script type="text/javascript" src="scripts/chili-1.7.pack.js"></script>
-    <link rel="stylesheet" type="text/css" media="screen" href="css/jq.css">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" href="../themes/default/default.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="../themes/pascal/pascal.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="../themes/orman/orman.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="../nivo-slider.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="style.css" type="text/css" media="screen" />
 </head>
 <body>
-	<div id="slider" class="pics">
-    	<?php
-        	//creamos las imagenes respectivas
-			$xmlFileName = "../../xml/plan.xml";
-			
-			if(isset($_REQUEST['id'])){
-				$xmlFileName = "../../xml/plan".$_REQUEST['id'].".xml";
-			}
+    <div id="wrapper">
+   		<div class="slider-wrapper theme-default">
+            <div class="ribbon"></div>
+            <div id="slider" class="nivoSlider">
+                <?php
+                	//creamos las imagenes respectivas
+					$xmlFileName = "../../xml/plan.xml";
+
+					if(isset($_REQUEST['id'])){
+						$xmlFileName = "../../xml/plan".$_REQUEST['id'].".xml";
+					}
 					
-            $doc = new DOMDocument();
-            $doc->load($xmlFileName);
+                	$doc = new DOMDocument();
+                	$doc->load($xmlFileName);
                 	
-            $secciones = $doc->getElementsByTagName("seccion");
-            $seccion = $secciones->item(3);
+                	$secciones = $doc->getElementsByTagName("seccion");
+                	$seccion = $secciones->item(3);
                 	
-            $items = $seccion->getElementsByTagName("item");
-            foreach($items as $item ){
-        ?>
-                <img src="./../../img/<?php echo $item->nodeValue;?>" alt=""  width="618px" height="300px" />
-        <?php                	 
-        	}
-        ?>
+                	$items = $seccion->getElementsByTagName("item");
+                	foreach($items as $item ){
+                ?>
+                		<img src="./../../img/<?php echo $item->nodeValue;?>" alt="" width="618px" height="300px" />
+                <?php                	 
+                	}
+                ?>
+            </div>
+        </div>
     </div>
-    
+
+    <script type="text/javascript" src="scripts/jquery-1.7.1.min.js"></script>
+    <script type="text/javascript" src="../jquery.nivo.slider.pack.js"></script>
     <script type="text/javascript">
     $(window).load(function() {
-    	$('#slider').cycle({ 
-    	    fx:    'turnDown', 
-    	    width: 618, 
-    	    height: 300, 
-    	    timeout: 1500 
-    	});
+        $('#slider').nivoSlider();
     });
     </script>
 </body>
