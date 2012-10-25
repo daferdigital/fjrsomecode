89176908568bacca67775e543df1daa814b1ba1b
felipe.rojasg@gmail.com <felipe.rojasg@gmail.com@deb3a35b-e1a3-a7ec-7b53-b241804db672>
2012-10-25 18:02:52 +0000

    agregada FK contra la tabla de mensajes

--- trunk/Uneweb/script_db/mide/mensaje_clientes.sql
+++ trunk/Uneweb/script_db/mide/mensaje_clientes.sql
@@ -4,3 +4,11 @@ CREATE  TABLE `mensaje_clientes` (
   `leido` CHAR(1) NOT NULL DEFAULT 0 ,
   PRIMARY KEY (`id_mensaje`, `id_cliente`) )
 ENGINE = InnoDB;
+
+ALTER TABLE `mensaje_clientes` 
+  ADD CONSTRAINT `FK_MENSAJE_CLIENTE`
+  FOREIGN KEY (`id_mensaje` )
+  REFERENCES `mensajes` (`id` )
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION
+, ADD INDEX `FK_MENSAJE_CLIENTE_idx` (`id_mensaje` ASC) ;
