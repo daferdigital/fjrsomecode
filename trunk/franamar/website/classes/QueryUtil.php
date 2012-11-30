7789eed60c0005b6e824a68412969b7b9d7e8787
felipe.rojasg@gmail.com <felipe.rojasg@gmail.com@deb3a35b-e1a3-a7ec-7b53-b241804db672>
2012-11-30 21:36:42 +0000

    Initial import.

---
+++ trunk/franamar/website/classes/QueryUtil.php
@@ -0,0 +1,5 @@
+<?php
+	class QueryUtil extends DBConexion{
+		
+	}
+?>
---
+++ trunk/franamar/website/includes/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+	<title>Franamar Logistica de Carga C.A.</title>
+</head>
+<body>
+	<table width="1150px" align="center">
+		<tr>
+			<td align="center">
+				<img title="" alt="header" src="images/header.png" />
+			</td>
+		</tr>
+		<tr>
+			<td>
+				
+			</td>
+		</tr>
+		<tr>
+		</tr>
+		<tr>
+		</tr>
+	</table>
+</body>
+</html>
