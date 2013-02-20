c6a441ec73fe4ad406602fa20cb031233d6605b4
felipe.rojasg <felipe.rojasg@gmail.com>
2013-02-20 23:57:20 +0000

    avances franamar

--- franamar/website/enlaces.php
+++ franamar/website/enlaces.php
@@ -20,11 +20,44 @@
         		
         		<br /><br />
         		
-        		<a class="enlacesInteres" href="http://www.bolipuertos.gob.ve" target="_blank">
-        			<img src="images/boliPuertos.png" /> Gobierno en L&iacute;nea
+        		<a class="enlacesInteres" href="http://www.gobiernoenlinea.ve" target="_blank">
+        			<img src="images/logoGobLinea.png" /> Gobierno en L&iacute;nea
         		</a>
-        		<a class="enlacesInteres" href="http://www.seniat.gob.ve/portal/page/portal/PORTAL_SENIAT" target="_blank">
-        			<img src="images/logoSENIAT.gif" /> SENIAT
+        		<a class="enlacesInteres" href="http://www.convertidordemedidas.com" target="_blank">
+        			<img src="images/logoConvertidorMedidas.jpg" /> Convertidor de Medidas
+        		</a>
+        		
+        		<br /><br />
+        		
+        		<a class="enlacesInteres" href="http://www.mppef.gob.ve/" target="_blank">
+        			<img src="images/logoMPPEF.png" /> Ministerio de Planificaci&oacute;n y Finanzas
+        		</a>
+        		<a class="enlacesInteres" href="http://www.wcoomd.org" target="_blank">
+        			<img src="images/logoWCO.png" /> Organizaci&oacute;n Mundial de Aduanas
+        		</a>
+        		
+        		<br /><br />
+        		
+        		<a class="enlacesInteres" href="http://www.wto.org/indexsp.htm" target="_blank">
+        			<img src="images/logoOMC.gif" /> Organizaci&oacute;n Mundial del Comercio
+        		</a>
+        		<a class="enlacesInteres" href="http://www.mercosur.int" target="_blank">
+        			<img src="images/logoMercosur.jpg" /> MERCOSUR
+        		</a>
+        		
+        		<br /><br />
+        		
+        		<a class="enlacesInteres" href="http://www.seniat.gob.ve/portal/page/portal/MANEJADOR_CONTENIDO_SENIAT/05MENU_HORIZONTAL/5.5ENLACES_DE_INTERES/ADUANAS_DEL_MUNDO.pdf" target="_blank">
+        			<img src="images/logoAdobePDF.jpg" /> Aduanas del Mundo
+        		</a>
+        		<a class="enlacesInteres" href="http://www.eurosocialfiscal.org" target="_blank">
+        			<img src="images/logoEuroSocial.gif" /> Eurosocial Fiscal
+        		</a>
+        		
+        		<br /><br />
+        		
+        		<a class="enlacesInteres" href="http://www.ciat.org" target="_blank">
+        			<img src="images/logoCIAT.jpg" /> Centro Interamericano de Administraciones Tributarias
         		</a>
       		</blockquote>
     	</td>
---
+++ franamar/website/noticias.php
@@ -0,0 +1,7 @@
+<?php include("includes/header.php");?>
+
+<div class="fondoAzul">
+      	<h1 class="blanca">Noticias</h1>
+</div>
+
+<?php include("includes/footer.php");?>
