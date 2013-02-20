0a00c7a44717f9eacc5e402acb3367a7ff7605c0
felipe.rojasg@gmail.com <felipe.rojasg@gmail.com@deb3a35b-e1a3-a7ec-7b53-b241804db672>
2013-02-20 23:48:56 +0000

    Initial import.

---
+++ trunk/quierounacompu/sis/doLogin.php
@@ -0,0 +1,8 @@
+<?php
+
+include ("conexion.php");
+
+$login = $_POST["login"];
+$clave = $_POST["clave"];
+	
+?>
---
+++ trunk/quierounacompu/sis/js/siteSIS.js
@@ -0,0 +1,26 @@
+String.prototype.trim=function(){
+	return this.replace(/^\s+|\s+$/g, '');
+};
+
+function validarLoginForm(loginForm){
+	var login = loginForm.login.value.trim();
+	var clave = loginForm.clave.value.trim();
+	
+	document.getElementById("formLogin").style.display = "none";
+	document.getElementById("formClave").style.display = "none";
+	
+	//vemos si no estan vacios los campos
+	var doSubmit = true;
+	if(login == ""){
+		doSubmit = false;
+		document.getElementById("formLogin").style.display = "inline";
+	}
+	if(clave == ""){
+		doSubmit = false;
+		document.getElementById("formClave").style.display = "inline";
+	}
+	
+	if (doSubmit) {
+		loginForm.submit();
+	}
+}
