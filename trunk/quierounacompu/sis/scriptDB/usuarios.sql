40f8bafbd32f975d810facd3eb22005f87d324a0
felipe.rojasg@gmail.com <felipe.rojasg@gmail.com@deb3a35b-e1a3-a7ec-7b53-b241804db672>
2013-02-25 11:52:45 +0000

    avances de quierounacompu

--- trunk/quierounacompu/sis/scriptDB/usuarios.sql
+++ trunk/quierounacompu/sis/scriptDB/usuarios.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE  IF NOT EXISTS `quierounacompu` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE  IF NOT EXISTS `quierounacompu` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `quierounacompu`;
 -- MySQL dump 10.13  Distrib 5.5.16, for Win32 (x86)
 --
 -- Host: localhost    Database: quierounacompu
 -- ------------------------------------------------------
--- Server version	5.5.8
+-- Server version	5.5.24
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -32,10 +32,7 @@ CREATE TABLE `usuarios` (
   `clave` varchar(45) NOT NULL COMMENT 'Clave debe quedar almacenada en MD5',
   `correo` varchar(100) NOT NULL,
   `tiempo_sesion` int(11) NOT NULL DEFAULT '10',
-  `id_rol` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_USER_ROL_idx` (`id_rol`),
-  CONSTRAINT `FK_USER_ROL` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -45,7 +42,7 @@ CREATE TABLE `usuarios` (
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
-INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `login`, `clave`, `correo`, `tiempo_sesion`, `id_rol`) VALUES (1,'Reynaldo','Dominguez','rDominguez','e10adc3949ba59abbe56e057f20f883e','rd@quierounacompu.com',10,1);
+INSERT INTO `usuarios` VALUES (1,'Felipe','Rojas','frojas','e10adc3949ba59abbe56e057f20f883e','fr@quierounacompu.com',20);
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -58,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2013-02-22 14:28:25
+-- Dump completed on 2013-02-25  7:20:22
