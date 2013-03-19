7b099261a1f7420cf0db9924435ea0f645209562
felipe.rojasg <felipe.rojasg@gmail.com>
2013-03-19 11:01:08 +0000

    cambios

--- quierounacompu/sis/ajax/updateEnvio.php
+++ quierounacompu/sis/ajax/updateEnvio.php
@@ -18,31 +18,26 @@ $statusEnvio = $envioDTO->getIdStatusActual();
 BitacoraDAO::registrarComentario("Ingreso en pagina ajax para vizualizar envio[".$recordId."]");
 
 //vemos el tipo de envio que se desea buscar o si se viene de busqueda avanzada
-if(isset($_POST["fromBusquedaAvanzada"])){
-	PageAccess::validateAccess(Constants::$OPCION_BUSQUEDA_AVANZADA);
+//venimos de las opciones especificas por cada tipo de envio
+//verificamos el permiso
+if(EnvioDAO::$COD_STATUS_NOTIFICADO == $statusEnvio){
+	PageAccess::validateAccess(Constants::$OPCION_EDICION_NOTIFICADOS);
 	$canEdit = true;
-}else{
-	//venimos de las opciones especificas por cada tipo de envio
-	//verificamos el permiso
-	if(EnvioDAO::$COD_STATUS_NOTIFICADO == $statusEnvio){
-		PageAccess::validateAccess(Constants::$OPCION_EDICION_NOTIFICADOS);
-		$canEdit = true;
-	} else if(EnvioDAO::$COD_STATUS_PAGO_CONFIRMADO == $statusEnvio){
-		PageAccess::validateAccess(Constants::$OPCION_EDICION_PAGOS_CONFIRMADOS);
-		$canEdit = true;
-	} else if(EnvioDAO::$COD_STATUS_PAGO_NO_ENCONTRADO == $statusEnvio){
-		PageAccess::validateAccess(Constants::$OPCION_EDICION_PAGOS_NO_ENCONTRADOS);
-		$canEdit = true;
-	} else if(EnvioDAO::$COD_STATUS_FACTURADO == $statusEnvio){
-		PageAccess::validateAccess(Constants::$OPCION_EDICION_FACTURADO);
-		$canEdit = true;
-	} else if(EnvioDAO::$COD_STATUS_PRESUPUESTADO == $statusEnvio){
-		PageAccess::validateAccess(Constants::$OPCION_ed);
-		$canEdit = true;
-	} else if(EnvioDAO::$COD_STATUS_ENVIADO == $statusEnvio){
-		PageAccess::validateAccess(Constants::$OPCION_EDICION_ENVIADO);
-		$canEdit = true;
-	}
+} else if(EnvioDAO::$COD_STATUS_PAGO_CONFIRMADO == $statusEnvio){
+	PageAccess::validateAccess(Constants::$OPCION_EDICION_PAGOS_CONFIRMADOS);
+	$canEdit = true;
+} else if(EnvioDAO::$COD_STATUS_PAGO_NO_ENCONTRADO == $statusEnvio){
+	PageAccess::validateAccess(Constants::$OPCION_EDICION_PAGOS_NO_ENCONTRADOS);
+	$canEdit = true;
+} else if(EnvioDAO::$COD_STATUS_FACTURADO == $statusEnvio){
+	PageAccess::validateAccess(Constants::$OPCION_EDICION_FACTURADO);
+	$canEdit = true;
+} else if(EnvioDAO::$COD_STATUS_PRESUPUESTADO == $statusEnvio){
+	PageAccess::validateAccess(Constants::$OPCION_ed);
+	$canEdit = true;
+} else if(EnvioDAO::$COD_STATUS_ENVIADO == $statusEnvio){
+	PageAccess::validateAccess(Constants::$OPCION_EDICION_ENVIADO);
+	$canEdit = true;
 }
 
 BitacoraDAO::registrarComentario("El usuario ".($canEdit ? "" : "NO")." puede editar el envio[".$recordId."]");
@@ -53,29 +48,69 @@ BitacoraDAO::registrarComentario("El usuario ".($canEdit ? "" : "NO")." puede ed
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 </head>
 <body>
-<div style="padding: 10px; width: 650px;" class="centered">
-	<table class="borderCollapse">
-		<tr>
-			<td>Fecha:</td>
-			<td><?php echo $result[0]["fecha"];?></td>
-		</tr>
-		<tr>
-			<td>Usuario:</td>
-			<td><?php echo $result[0]["nombre"]." ".$result[0]["apellido"];?></td>
-		</tr>
-		<tr>
-			<td>Error?:</td>
-			<td><?php echo $result[0]["was_error"] == 0 ? "No" : "S&iacute;";?></td>
-		</tr>
-		<tr>
-			<td>Consulta:</td>
-			<td><?php echo $result[0]["query"];?></td>
-		</tr>
-		<tr>
-			<td>Resultado:</td>
-			<td><?php echo $result[0]["result"];?></td>
-		</tr>
-	</table>	
+<div style="padding: 10px; width: 850px;" class="centered">
+	<div id="myjquerymenu" class="jquerycssmenu" style="height: 25px;">
+		<ul>
+			<li><a href="#tabs-0">Datos de Compra</a></li>
+			<li><a href="#tabs-1">Datos de Pago</a></li>
+			<li><a href="#tabs-2">Datos para el env&iacute;o</a></li>
+			<li><a href="#tabs-3">Envio desde Quierounacompu</a></li>
+			<li><a href="#tabs-4">Status Final y Comentarios</a></li>
+		</ul>
+		<br style="clear: both;" />
+		<div id="tabs-0" style="background-color: white;">
+			<br />
+			<br />
+			<table>
+				<tr>
+					<td>Nombre:</td>
+					<td><?php echo $envioDTO->getNombreCompleto();?></td>
+				</tr>
+				<tr>
+					<td>Seudonimo MercadoLibre:</td>
+					<td><?php echo $envioDTO->getSeudonimoML();?></td>
+				</tr>
+				<tr>
+					<td>C.I. RIF:</td>
+					<td><?php echo $envioDTO->getCiRIF();?></td>
+				</tr>
+				<tr>
+					<td>Correo:</td>
+					<td><?php echo $envioDTO->getCorreo();?></td>
+				</tr>
+				<tr>
+					<td>Detalle de la Compra:</td>
+					<td><?php echo $envioDTO->getDetalleCompra();?></td>
+				</tr>
+			</table>
+		</div>
+		<div id="tabs-1" style="background-color: white;">
+			<br />
+			<br />
+			<table>
+				<tr>
+					<td>Banco:</td>
+					<td><?php echo $envioDTO->getDescBanco();?></td>
+				</tr>
+				<tr>
+					<td>Medio de Pago:</td>
+					<td><?php echo $envioDTO->getDescMedioPago();?></td>
+				</tr>
+				<tr>
+					<td>Fecha del pago:</td>
+					<td><?php echo $envioDTO->getFechaPago();?></td>
+				</tr>
+				<tr>
+					<td>Vauche:</td>
+					<td><?php echo $envioDTO->getNumVoucher();?></td>
+				</tr>
+				<tr>
+					<td>Monto:</td>
+					<td><?php echo $envioDTO->getMontoPago();?></td>
+				</tr>
+			</table>
+		</div>
+	</div>
 </div>
 </body>
 </html>
