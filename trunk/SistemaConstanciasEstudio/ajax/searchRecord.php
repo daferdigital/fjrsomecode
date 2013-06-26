974db84ad71020a26f50d2c22738279412f86c4d
felipe.rojasg@gmail.com <felipe.rojasg@gmail.com@deb3a35b-e1a3-a7ec-7b53-b241804db672>
2013-06-26 18:04:31 +0000

    finalizada version 1

--- trunk/SistemaConstanciasEstudio/ajax/searchRecord.php
+++ trunk/SistemaConstanciasEstudio/ajax/searchRecord.php
@@ -25,7 +25,7 @@ $pageRecords = DBUtil::getRecordsByPage($query, $pageNumber, $maxRecordsNumbers)
 $pagingDAO = new PagingDAO($pageNumber, $scriptFunction, $totalRecords);
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 </head>
