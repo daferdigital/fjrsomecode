e224f5efafc4c00e574f56ce04cfbf8ddd03d06a
felipe.rojasg <felipe.rojasg@gmail.com>
2013-11-19 17:56:16 +0000

    se omiten las vistas en el calculo de indices

--- parlay/buildIndexes.php
+++ parlay/buildIndexes.php
@@ -11,17 +11,23 @@
 	
 	$query = "SHOW TABLES";
 	$analyzeQuery = "ANALYZE TABLE ";
+	$time0 = time();
 	
 	$result = DBUtil::executeSelect($query);
 	foreach ($result as $tableName){
-		$result1 = DBUtil::executeSelect($analyzeQuery.$tableName[0]);
 ?>
 	<tr>
 		<td><?php echo $tableName[0];?></td>
 		<td>
 <?php		
-		foreach ($result1 as $salida){
-			echo $salida[2]."<br />".$salida[3];
+		if(substr($tableName[0], 0, 6) === "vista_"){
+			echo "Es una vista, no se procesa.";
+		} else {
+			$result1 = DBUtil::executeSelect($analyzeQuery.$tableName[0]);
+			foreach ($result1 as $salida){
+				echo $salida[2]."<br />".$salida[3]." (".(time() - $time0)." s)";
+			}
+			$time0 = time();
 		}
 ?>
 		</td>
