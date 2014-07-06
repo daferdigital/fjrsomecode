800818ce8ff19690acb0694213e5f378c8148bed
felipe.rojasg@gmail.com <felipe.rojasg@gmail.com@deb3a35b-e1a3-a7ec-7b53-b241804db672>
2014-07-06 17:10:52 +0000

    ajustes en el reporte de ganancias y perdidas en todos los niveles

--- trunk/parlay/reportes/reporte_1.php
+++ trunk/parlay/reportes/reporte_1.php
@@ -7,9 +7,6 @@ include('../procesos/conexion.php');
 include_once('fechas.php'); 
 //	$fecha_desde=formato_sql($_GET['fdesde'],'/');
 ///	$fecha_hasta= formato_sql($_GET['fhasta'],'/');
-
-
-
 if ($_GET["fdesde"]!=''){
 	$fecha_desde1= formato_sql($_GET['fdesde'],'/');
 	$fecha_hasta1=formato_sql($_GET['fhasta'],'/');
@@ -40,7 +37,7 @@ if($fecha_desde=='' && $fecha_hasta=='' && $id_banquero=='' && $id_intermediario
 	
 }else if($id_banquero!='' && $id_intermediario=='' && $id_taquilla=='' && ($_SESSION['perfil']=='1' || $_SESSION['perfil']=='2')){
 	//INTERMEDIARIO (INTERMEDIARIOS DEL BANQUERO)
-	$select=", i.*";
+	$select=", i.* ";
 	$intermediario=", i.pp, i.pd";
 	$nivel_muestra='2';
 	$where="and i.idbanquero=$id_banquero";
@@ -50,8 +47,7 @@ if($fecha_desde=='' && $fecha_hasta=='' && $id_banquero=='' && $id_intermediario
 }else if(($id_intermediario!='' || $id_intermediario=='') && $id_taquilla=='' &&( $_SESSION['perfil']=='1' || $_SESSION['perfil']=='2' || $_SESSION['perfil']=='3' || $_SESSION['perfil']=='4')){
 	//lista taquillas del intermediario..	
 	$select=", i.*, t.*, SUM(CASE vsv.pagado WHEN '1' THEN vsv.monto_real_pagar END) AS pre_pagados, 
-			SUM(CASE vsv.vencido WHEN '1' THEN vsv.monto_real_pagar END) AS pre_no_pagados, 
-			SUM(CASE vsv.reembolsar WHEN '1' THEN /*vsv.apuesta*/0 END) AS devolucion";
+			SUM(CASE vsv.vencido WHEN '1' THEN vsv.monto_real_pagar END) AS pre_no_pagados";
 	$nivel_muestra='3';
 		if($_SESSION['perfil']=='3'){
 			$where="and i.idintermediario='".$_SESSION['datos']['idintermediario']."' ";
@@ -89,7 +85,8 @@ if($nivel_muestra!='4'){
  $sql="SELECT vsv.*, i.idintermediario, b.idbanquero, i.nombre, 
 		SUM(CASE WHEN vsv.cantidad_apuesta >= '2' THEN vsv.apuesta END) AS ventas_parlay,
 		SUM(CASE WHEN vsv.cantidad_apuesta = '1' THEN vsv.apuesta END) AS ventas_derecho, 
-		SUM(CASE WHEN vsv.ganador = '1' THEN vsv.monto_real_pagar WHEN vsv.recalculado = '1' THEN vsv.monto_real_pagar WHEN vsv.reembolsar = '1' THEN vsv.monto_real_pagar END) AS premios,
+		SUM(CASE WHEN vsv.reembolsar = '1' THEN 0/*vsv.monto_real_pagar*/ WHEN vsv.ganador = '1' THEN vsv.monto_real_pagar WHEN vsv.recalculado = '1' THEN vsv.monto_real_pagar END) AS premios,
+		SUM(CASE vsv.reembolsar WHEN '1' THEN vsv.apuesta END) AS devolucion,
 		SUM(vsv.apuesta) as total	$select $taquilla
 	FROM ventas vsv 
 	LEFT JOIN taquillas t ON (vsv.idtaquilla=t.idtaquilla)	
@@ -108,6 +105,7 @@ $sql="SELECT vsv.*, i.idintermediario, b.idbanquero, i.nombre, t.*
 	ORDER BY idventa";
 	////(select SUM(apuesta) FROM ventas WHERE anulado='0' AND fecha BETWEEN '$fecha_desde1' AND '$fecha_hasta1' $where1  GROUP BY idtaquilla)  AS total
 }
+echo "<!-- $sql -->";
 $res=mysql_query($sql);?>
 
 <?
@@ -123,18 +121,17 @@ if($nivel_muestra=='1'){
       <!--<div style="background:#FFF; font-size:16px; text-align:center;"> fechas desde 01/06/2012 - 20/06/2012</div>-->
      <table width="1000" border="1" cellspacing="0" cellpadding="0" align="center" bgcolor="#FFFFFF">
       <tr>
-        <td colspan="7" style="color:#03F; font-size:16px; height:40px; font-weight:bold; text-align:right"><br />
+        <td colspan="8" style="color:#03F; font-size:16px; height:40px; font-weight:bold; text-align:right"><br />
         Nivel: Banqueros</td>
        </tr>
       <tr>
         <td width="50" bgcolor="#CCCCCC"><div align="center">ID</div></td>
         <td width="273" bgcolor="#CCCCCC"><div align="center">BANCA</div></td>
         <td width="150" bgcolor="#66FF33"><div align="center"><strong>TOTAL VENTAS</strong></div></td>
-        <td width="128" bgcolor="#CCCCCC"><div align="center">VENTAS <br />
-        PARLAY</div></td>
-        <td width="107" bgcolor="#CCCCCC"><div align="center">VENTAS <br />
-        DERECHO</div></td>
+        <td width="128" bgcolor="#CCCCCC"><div align="center">VENTAS <br />PARLAY</div></td>
+        <td width="107" bgcolor="#CCCCCC"><div align="center">VENTAS <br />DERECHO</div></td>
         <td width="159" bgcolor="#CCCCCC"><div align="center">PREMIOS</div></td>
+        <td width="159" bgcolor="#CCCCCC"><div align="center">DEVOLUCIONES</div></td>
         <td width="117" bgcolor="#CCCCCC"><div align="center">SALDO</div></td>
       </tr><? 
 	if ($row=mysql_fetch_array($res)) {
@@ -145,6 +142,7 @@ if($nivel_muestra=='1'){
 		$ventas_parlay=$row["ventas_parlay"];
 		$ventas_derecho=$row["ventas_derecho"];
 		$premios=$row["premios"];
+		$devolucion=$row["devolucion"];
 		$i++;?>  
       <tr>
         <td><div align="center"><? echo $i;?></div></td>
@@ -155,16 +153,16 @@ if($nivel_muestra=='1'){
         <td bgcolor="#66FF33"><div align="right"><? echo $ventas;?> Bs.</div></td>
         <td><div align="right"><? echo $ventas_parlay;?> Bs.</div></td>
         <td><div align="right"><? if($ventas_derecho<='0'){echo "0";}else{echo $ventas_derecho;}?> Bs.</div></td>
-        <td><div align="right"><? if($premios>='1'){echo $premios;}else{ echo "0";} premios ?> Bs.</div></td>
-        <td><? $saldo=($ventas-$premios);?>
-        <div align="right" <? if($saldo<0){echo "style='color: #F00;'";}?>><? echo $saldo;?> Bs.</div></td>
+        <td><div align="right"><? if($premios>='1'){echo $premios;}else{ echo "0";} ?> Bs.</div></td>
+        <td><div align="right"><? if($devolucion>='1'){echo $devolucion;}else{ echo "0";} ?> Bs.</div></td>
+        <td><? $saldo=($ventas-$premios-$devolucion);?><div align="right" <? if($saldo<0){echo "style='color: #F00;'";}?>><? echo $saldo;?> Bs.</div></td>
       </tr><?
 	  $ventas_total=$ventas_total+$ventas;
 	  $totalVentas_parlay=$totalVentas_parlay+$ventas_parlay;
 	  $totalVentas_derecho=$totalVentas_derecho+$ventas_derecho;
 	  $totalPremios=$totalPremios+$premios;
 	  $totalSaldo=$totalSaldo+$saldo;
-	  
+	  $totalDevoluciones=$totalDevoluciones+$devolucion;
 	}while ($row=mysql_fetch_array($res));?>
       <tr>
         <td>&nbsp;</td>
@@ -173,6 +171,7 @@ if($nivel_muestra=='1'){
         <td><div align="right"><strong><? echo number_format($totalVentas_parlay, 2, '.', '');?> Bs.</strong></div></td>
         <td><div align="right"><strong><? echo number_format($totalVentas_derecho, 2, '.', '');?> Bs.</strong></div></td>
         <td><div align="right"><strong><? echo number_format($totalPremios, 2, '.', '');?> Bs.</strong></div></td>
+        <td><div align="right"><strong><? echo number_format($totalDevoluciones, 2, '.', '');?> Bs.</strong></div></td>
         <td>
         <div align="right" <? if($totalSaldo<0){echo "style='color: #F00;'";}?>>
         	<strong><? echo $totalSaldo;?> Bs.</strong>
@@ -192,7 +191,7 @@ if($nivel_muestra=='1'){
            <a class="ajax_reporte" href="reportes/reporte_1.php?fdesde=<?Php echo $_GET['fdesde'];?>&fhasta=<?Php echo $_GET['fhasta'];?>">Banqueros</a><br />
         <br />
         </div></td>
-        <td colspan="7" style="color:#03F; font-size:16px; height:40px; font-weight:bold; text-align:right"><br />
+        <td colspan="8" style="color:#03F; font-size:16px; height:40px; font-weight:bold; text-align:right"><br />
         Nivel: Intermediarios</td>
        </tr>
       <tr>
@@ -204,6 +203,7 @@ if($nivel_muestra=='1'){
         <td width="100" bgcolor="#CCCCCC"><div align="center">% PARLAY</div></td>
         <td width="111" bgcolor="#CCCCCC"><div align="center">% DERECHO</div></td>
         <td width="109" bgcolor="#CCCCCC"><div align="center">PREMIOS</div></td>
+        <td width="109" bgcolor="#CCCCCC"><div align="center">DEVOLUCIONES</div></td>
         <td width="88" bgcolor="#CCCCCC"><div align="center">SALDO</div></td>
       </tr><? 
 	if ($row=mysql_fetch_array($res)) {
@@ -213,6 +213,7 @@ if($nivel_muestra=='1'){
 		$ventas_parlay=$row["ventas_parlay"];
 		$ventas_derecho=$row["ventas_derecho"];
 		$premios=$row["premios"];
+		$devoluciones=$row["devolucion"];
 		$i++;
 		?>            
       <tr>
@@ -231,8 +232,11 @@ if($nivel_muestra=='1'){
         <td><div align="right">
           <? if($premios>='1'){echo $premios;}else{echo "0";} ?> 
         Bs.</div></td>
+        <td><div align="right">
+          <? if($devoluciones>='1'){echo $devoluciones;}else{echo "0";} ?> 
+        Bs.</div></td>
         <td>
-        <? $saldo=($ventas-$premios-$por_parlay-$por_derecho);?>
+        <? $saldo=($ventas-$premios-$por_parlay-$por_derecho-$devoluciones);?>
         <div align="right" <? if($saldo<0){echo "style='color: #F00;'";}?>><? echo $saldo;?> Bs.</div></td>
       </tr><?
   	  $ventas_total=$ventas_total+$ventas;
@@ -240,7 +244,7 @@ if($nivel_muestra=='1'){
 	  $totalVentas_derecho=$totalVentas_derecho+$ventas_derecho;
 	  $totalPremios=$totalPremios+$premios;
 	  $totalSaldo=$totalSaldo+$saldo;
-
+	  $totalDevolucion=$totalDevolucion+$devoluciones;
 	}while ($row=mysql_fetch_array($res));?>
       <tr>
         <td>&nbsp;</td>
@@ -251,6 +255,7 @@ if($nivel_muestra=='1'){
         <td><div align="right"></div></td>
         <td align="center"><div align="right"></div></td>
         <td><div align="right"><strong><? echo number_format($totalPremios, 2, '.', '');?> Bs.</strong></div></td>
+        <td><div align="right"><strong><? echo number_format($totalDevolucion, 2, '.', '');?> Bs.</strong></div></td>
         <td><div align="right" <? if($totalSaldo<0){echo "style='color: #F00;'";}?>>
         	<strong><? echo $totalSaldo;?> Bs.</strong>
         </div></td>
@@ -281,7 +286,7 @@ if($nivel_muestra=='1'){
         <td width="73" bgcolor="#CCCCCC"><div align="center">VENTAS DERECHO</div></td>
         <td width="61" bgcolor="#CCCCCC"><div align="center">% PARLAY</div></td>
         <td width="85" bgcolor="#CCCCCC"><div align="center">% DERECHO</div></td>
-        <!-- <td width="72" bgcolor="#CCCCCC" style="font-size:11px;"><div align="center">DEVOLUCI&Oacute;N</div></td> -->
+        <td width="72" bgcolor="#CCCCCC" style="font-size:11px;"><div align="center">DEVOLUCI&Oacute;N</div></td>
         <td width="80" bgcolor="#CCCCCC"><div align="center">PREMIOS</div></td>
         <td width="85" bgcolor="#CCCCCC"><div align="center">PREMIOS PAGADOS</div></td>
         <td width="77" bgcolor="#CCCCCC"><div align="center">PREMIOS <br />SIN PAGAR</div></td>
@@ -318,13 +323,7 @@ if($nivel_muestra=='1'){
         <td style="font-size:11px;"><div align="right"><? if($ventas_derecho<='0'){echo "0";}else{echo $ventas_derecho;}?> Bs.</div></td>
         <td style="font-size:11px;"><div align="right"><? echo $por_parlay=($row["pdv"]/100)*$ventas_parlay;?> Bs.</div></td>
         <td style="font-size:11px;"><div align="right"><? echo $por_derecho=($row["pdvd"]/100)*$ventas_derecho;?> Bs.</div></td>
-        <!-- 
-        <td style="font-size:11px;">
-        	<div align="right">
-          		<? if($devolucion>='1'){echo $devolucion;}else{echo "0";} ?> Bs.
-          	</div>
-        </td>
-        -->
+        <td style="font-size:11px;"><div align="right"><? if($devolucion>='1'){echo $devolucion;}else{echo "0";} ?> Bs.</div></td>
         <td style="font-size:10px;"><div align="right"><? if($premios>='1'){echo $premios;}else{echo "0";} ?> Bs.</div></td>
         <td style="font-size:10px;"><div align="right">
 
@@ -360,13 +359,8 @@ if($nivel_muestra=='1'){
         <td style="font-size:11px;"><div align="right"><strong><? echo number_format($totalVentas_derecho, 2, '.', '');?> Bs.</strong></div></td>
         <td style="font-size:11px;"><div align="right"></div></td>
         <td style="font-size:11px;"><div align="right"></div></td>
-        <!--
-        <td style="font-size:11px;">
-        	<div align="right">
-        		<strong><span style="font-size:11px;"><? echo number_format($totalDevoluciones, 2, '.', '');?> Bs.</span></strong>
-        	</div>
-        </td>
-        -->
+        <td style="font-size:11px;"><div align="right">
+        <strong><span style="font-size:11px;"><? echo number_format($totalDevoluciones, 2, '.', '');?> Bs.</span></strong></div></td>
         <td style="font-size:10px;"><div align="right">
         <strong><span style="font-size:11px;"><? echo number_format($totalPremios, 2, '.', '');?> Bs.</span></strong></div></td>
         <td style="font-size:10px;"><div align="right"></div></td>
@@ -458,6 +452,7 @@ if($nivel_muestra=='1'){
         </div></td>
       </tr> <?
 	  $monto_pagar=$monto_pagar+$row['monto_real_pagar'];
+	  $total1=$total1+$apuesta1;
 	}while ($row=mysql_fetch_array($res));?>
       <tr>
         <td colspan="4" style="font-size:11px;">&nbsp;</td>
